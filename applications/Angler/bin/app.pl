72d4585e06a66584cc382c4c603db6ff30191972
Sam Batschelet <sam@westbranchresort.com>
2013-06-01 16:09:49 -0400

    * Few fixes to paths and a couple new modules

--- applications/Angler/bin/app.pl
+++ applications/Angler/bin/app.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/env perl
 
-use FindBin;
-use lib "$FindBin::Bin/../../../nitesi/lib", 
-	"$FindBin::Bin/../../../nitesi-dancer/lib";
+#use FindBin;
+#use lib "$FindBin::Bin/../../../nitesi/lib", 
+#	"$FindBin::Bin/../../../nitesi-dancer/lib";
+
+use lib "/home/sam/camp39/local/lib/perl5";
 
 use Dancer;
 use Angler;
