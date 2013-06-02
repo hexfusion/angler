9c66fca39b2f48a38ce0d0c1838d39f2972dd6ee
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-06-02 09:19:02 -0400

    Add Nitesi plugin to Angler main module.

--- applications/Angler/lib/Angler.pm
+++ applications/Angler/lib/Angler.pm
@@ -1,10 +1,14 @@
 package Angler;
 use Dancer ':syntax';
+use Dancer::Plugin::Nitesi;
+use Dancer::Plugin::Nitesi::Routes;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
 };
+
+shop_setup_routes;
 
 true;
