5f9e0836fd917e6652241e6c961ecc326d10d3cc
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-09-09 04:50:44 -0400

    Add hooks with country selector.

--- applications/Angler/lib/Angler.pm
+++ applications/Angler/lib/Angler.pm
@@ -8,8 +8,42 @@ our $VERSION = '0.1';
 hook 'before_layout_render' => sub {
     my $tokens = shift;
 
+    # display cart count
     $tokens->{cart_count} = cart->count;
+
+    # navigation elements
+    $tokens->{navigation} = shop_navigation->search(where => {parent => 0});
 };
+
+hook 'before_product_display' => sub {
+    my ($product) = @_;
+    
+    debug "Before product display: ", $product->sku;
+};
+
+hook 'before_cart_display' => sub {
+    my ($values) = @_;
+
+    $values->{countries} = countries();
+    $values->{country} = 'US';
+};
+
+hook 'before_checkout_display' => sub {
+    my ($values) = @_;
+
+    $values->{countries} = countries();
+    $values->{country} = 'US';
+};
+
+sub countries {
+    return query->select(
+                join => "country",
+                fields => [
+                        "selector AS value", "name AS label"
+                ],
+                order => "name"
+        );
+}
 
 get '/' => sub {
     template 'home';
