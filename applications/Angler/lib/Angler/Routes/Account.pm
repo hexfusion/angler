b5ebc85747a8f162bf93f0aa6432883ae28c2289
Sam Batschelet <sbatschelet@mac.com>
2013-11-25 14:30:08 -0500

    Add Facebook::Graph auth with DPAE

--- applications/Angler/lib/Angler/Routes/Account.pm
+++ applications/Angler/lib/Angler/Routes/Account.pm
@@ -8,7 +8,8 @@ use Dancer::Plugin::Auth::Extensible qw(
 logged_in_user authenticate_user user_has_role require_role
 require_login require_any_role
 );
-
+use String::Random;
+use Facebook::Graph;
 use Input::Validator;
 use DateTime qw();
 use DateTime::Duration qw();
@@ -27,7 +28,7 @@ get '/registration' => sub {
 };
 
 post '/registration' => sub {
-    my ($form, $values, $validator, $error_ref, $acct, $user, $role, $user_data, $user_role_id );
+    my ($form, $values, $validator, $error_ref, $acct, $attr, $user, $role, $user_data, $user_role_id );
 
     $form = form('registration');
 
@@ -39,7 +40,6 @@ post '/registration' => sub {
                       password => $values->{password},
                       created => $now
         };
-
     # validate form input
     $validator = new Input::Validator;
 
@@ -60,8 +60,12 @@ post '/registration' => sub {
     #debug("Register account: $values->{email}.");
     $acct = rset('User')->create( $user_data );
 
-    # add role
+    # add user role
     $role = rset('UserRole')->create( { users_id => $acct->id, roles_id => $user_role_id  } );
+
+    # add user attribute
+    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'facebook', value => '0' } );
+#    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'fb_token', value => '' } );
 
     #debug("Register result: ", $acct || 'N/A');
     return redirect '/login';
@@ -104,4 +108,77 @@ get '/logout' => sub {
     return redirect '/';
 };
 
+get '/login/denied' => sub {
+    template 'login_denied';
+};
+
+get '/login' => sub {
+    template 'login';
+};
+
+get '/forum' => require_login sub {
+    template 'forum';
+     };
+
+get '/account' => require_role users => sub {
+    template 'account_my-account';
+};
+
+get '/facebook/login' => sub {
+    my $fb = Facebook::Graph->new( config->{facebook} );
+    #redirect $fb->authorize->uri_as_string;
+        redirect $fb
+        ->authorize
+        ->extend_permissions( qw(offline_access email publish_stream) )
+        ->uri_as_string;
+};
+get '/facebook/postback/' => sub {
+    my ( $attr, $avail,  $user, $user_email, $fb, $token_response_object, $authorization_code, $role, $acct, $user_role_id, $pass, $secret, $user_data);
+
+    $authorization_code = params->{code};
+    $fb                 = Facebook::Graph->new( config->{facebook} );
+    $user_role_id = '3';
+    $pass = new String::Random;
+    # create password for facebook user
+    $secret = $pass->randpattern("CCcn!Ccn");
+    $token_response_object = $fb->request_access_token($authorization_code);
+
+    # get fb user information
+    $user = $fb->fetch('me');
+    $user_email = $user->{email}; 
+    $avail = rset('User')->find({ email => $user_email });
+
+    # if the customer is not already registered with this email then DO IT.
+    if ( !$avail ) {
+
+    $user_data = { username => $user->{email},
+                   first_name => $user->{first_name},
+                   last_name => $user->{last_name},
+                   email    => $user_email,
+                   password => $secret,
+                   created => $now
+    };
+
+    # create new user
+    $acct = rset('User')->create( $user_data );
+
+    # add user role
+    $role = rset('UserRole')->create( { users_id => $acct->id, roles_id => $user_role_id  } );
+
+    # add user attribute
+    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'facebook', value => '1' } );
+    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'fb_token', value => $token_response_object->token } );
+    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'fb_token_exp', value => $token_response_object->expires } );
+    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'fb_id', value => $user->{id} } );
+    $attr = rset('UserAttribute')->create( { users_id => $acct->id, name => 'fb_password', value => $secret } );
+}
+    # log person in with DPAE
+    session logged_in_user => $user->{email};
+    session logged_in_user_realm => 'users';
+
+    # TODO lets email the user now with login information
+
+    redirect '/';
+
+};
 1;
