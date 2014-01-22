f165258a36717f388e146766c77b926e81f174c4
Sam Batschelet <sbatschelet@mac.com>
2014-01-22 09:05:53 -0500

    Add Review route

---
+++ applications/Angler/lib/Angler/Routes/Review.pm
@@ -0,0 +1,99 @@
+package Angler::Routes::Review;
+
+use Dancer ':syntax';
+use Dancer::Plugin::Interchange6;
+use Dancer::Plugin::Form;
+use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Auth::Extensible qw(
+logged_in_user authenticate_user user_has_role require_role
+require_login require_any_role
+);
+use Data::Transpose::Validator;
+use Try::Tiny;
+
+get '/review' => require_login sub {
+    my $form = form('review');
+     $form->reset;
+    template 'review', {form => $form };
+};
+get '/review_thank-you' => sub {
+    my $form = form('review');
+    template 'review_thank-you', {form => $form };
+};
+
+
+
+post '/review' => require_login sub {
+    my $form = form('review');
+    my $values = $form->values;
+    my $user = logged_in_user;
+    my $users_id = $user->users_id;
+
+    my ($form_validate, $error_string) = validate_review($form, $values, $users_id);
+
+    unless ($error_string) {
+        my $review_data = { rating => $values->{rating},
+                            title => $values->{title},
+                            sku => $values->{sku},
+                            content => $values->{content},
+                            recommend => $values->{recommend},
+                            users_id => $users_id,
+        };
+
+        # add review
+        my $review = rset('Review')->create( $review_data );
+
+        # add/use display name
+        $user->add_attribute('user_alias',$values->{user_alias});
+        $user->add_attribute('user_alias_visable',$values->{user_alias_visable});
+
+        return redirect '/review_thank-you';
+    }
+    else {
+    template 'review', {form => $form_validate,
+                    errors => $error_string};
+    }
+};
+
+sub validate_review {
+    my ( $form, $values, $users_id) = @_;
+
+    # validate form input
+    my $validator = Data::Transpose::Validator->new(requireall => 1);
+
+    $validator->field('rating' => "String");
+    $validator->field('title' => "String");
+    $validator->field('sku' => "String");
+#    $validator->field('sku' => "ProductSku");
+#    $validator->field('user_alias' => "String");
+#    $validator->field('user_alias_visable' => "String");
+#    $validator->field('recommend' => "String");
+    $validator->field('content' => "String");
+
+    my $clean = $validator->transpose($values);
+    my $error_string;
+
+    if (!$clean || $validator->errors) {
+        my $error_ref = $validator->errors;
+        debug("Register errors: ", $error_ref);
+        $error_string = $validator->packed_errors;
+        debug("Error string: ", $error_string);
+        $form->errors($error_string);
+        debug to_dumper($error_ref);
+        $form->fill($values);
+    }
+    else { 
+        my $sku = $values->{sku};
+        my $review_exist = rset('Review')->find({sku => $sku, users_id => $users_id});
+        if ($review_exist) {
+            die "A review for $sku already exists for this user!";
+            #$form->errors({sku => "A review for $sku already exists for this user!"});
+            #$error_string=({sku => 'Valid sku required for review'});
+        }
+#     $form->fill($values);
+    }
+    return ($form, $error_string);
+};
+
+
+1;
