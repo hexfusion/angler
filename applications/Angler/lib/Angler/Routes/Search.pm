7ddaf884006008dff3e816273efecbd58ea3a370
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-02-06 05:59:07 -0500

    Remove debug message and superfluous whitespace.

--- applications/Angler/lib/Angler/Routes/Search.pm
+++ applications/Angler/lib/Angler/Routes/Search.pm
@@ -8,7 +8,7 @@ get '/search' => sub {
     debug "Searching for : ", param('q');
 
     my @words = split(/\s+/, param('q'));
-    
+
     # create search object
     my $search = Angler::Search->new(
         solr_url => config->{solr_url},
@@ -17,11 +17,9 @@ get '/search' => sub {
     my $results = $search->solr_query;
     my $count = $search->count;
 
-    debug "Results: ", $results;
-    
     template 'product-listing', {products => $results,
                                  count => $count};
 };
-  
+
 1;
 
