7216f7dcd0ede654ca261d2563f71b45e107dc6a
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-02-25 14:56:38 -0500

    Add support for credit card validation and iterators. Add shop_charge call for credit card payments.

--- applications/Angler/lib/Angler/Routes/Checkout.pm
+++ applications/Angler/lib/Angler/Routes/Checkout.pm
@@ -4,6 +4,8 @@ use Dancer ':syntax';
 use Dancer::Plugin::Interchange6;
 use Dancer::Plugin::Form;
 
+use DateTime;
+
 get '/checkout' => sub {
     my $form;
 
@@ -28,10 +30,35 @@ post '/checkout' => sub {
         debug "Error hash: ", $error_hash;
     }
     else {
-        # complete order
+        # input data complete, charge amount
+        my $expiration = sprintf(
+            "%02d/%02d",
+            $values->{card_month}, $values->{card_year});
+
+        my %payment_data = (amount => cart->total,
+                     first_name => $values->{first_name},
+                     last_name => $values->{last_name},
+                     card_number => $values->{card_number},
+                     expiration => $expiration,
+                     cvc => $values->{card_cvc});
+
+        $payment_data{action} = 'Normal Authorization';
+        
+        debug("Payment_data: ", \%payment_data);
+        
+	    my $tx = shop_charge(%payment_data);
+
+        if ($tx->is_success) {
+            debug "Payment successful: ", $tx->authorization;
+        }
+        else {
+            debug "Payment failed: ", $tx->error_message;
+        }
+
+        
         # ...
 
-        Log("Order complete.");
+        debug("Order complete.");
     }
 
     $form->fill($values);
@@ -45,6 +72,7 @@ sub validate_checkout {
     # validate form input
     my $validator = Data::Transpose::Validator->new(requireall => 1);
 
+    # shipping address
     $validator->field('first_name' => "String");
     $validator->field('last_name' => "String");
     $validator->field('address' => "String");
@@ -53,6 +81,20 @@ sub validate_checkout {
 
     # credit card data
     $validator->field('card_number' => 'CreditCard');
+    $validator->field('card_month' =>
+                          {validator => 'NumericRange',
+                           options => {
+                               min => 1,
+                               max => 12,
+                           }
+                       });
+    $validator->field('card_cvc' =>
+                          {validator => 'NumericRange',
+                           options => {
+                               min => 1,
+                               max => 999,
+                           }
+                       });
 
     if (! $validator->transpose($values)) {
         my ($v_hash, %errors);
@@ -76,6 +118,10 @@ sub checkout_tokens {
     # iterator for countries
     $tokens->{countries} = [shop_country->search({active => 1})];
 
+    # iterators for credit card expiration
+    $tokens->{card_months} = card_months();
+    $tokens->{card_years} = card_years();
+
     if ($errors) {
         $tokens->{errors} = $errors;
     }
@@ -87,5 +133,27 @@ sub checkout_tokens {
 
     return $tokens;
 };
+
+# iterators for credit card expiration
+sub card_months {
+    my @months;
+
+    for my $i (1..12) {
+        push @months, {value => $i, label => $i};
+    }
+
+    return \@months;
+}
+
+sub card_years {
+    my @years;
+    my $cur_year = DateTime->now->year;
+
+    for my $i ($cur_year..$cur_year+20) {
+        push @years, {value => substr($i,0,2), label => $i};
+    }
+
+    return \@years;
+}
 
 1;
