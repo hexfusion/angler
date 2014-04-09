acfa93b6ded0e55d56213326ca01e01069be4535
Sam Batschelet <sbatschelet@mac.com>
2014-04-08 23:04:02 -0400

    Add some pod and small other small fixes.

--- applications/Angler/lib/Angler/Tax.pm
+++ applications/Angler/lib/Angler/Tax.pm
@@ -3,7 +3,7 @@ package Angler::Tax;
 use strict;
 use warnings;
 
-=head2 tax_location($schema, $state);
+=head2 taxable_location($schema, $state);
 
 returns 0|1
 
@@ -21,12 +21,19 @@ sub rate {
     return $tax_total;
 }
 
+=head2 rate($schema, $state, $subtotal);
+
+returns tax amount.
+
+=cut
+
 sub taxable_location {
     my ($schema, $state) = @_;
-    my $state_iso_code = $state->state_iso_code;
+
         unless ($state) {
-            die "taxable location requires a state";
+            return 0;
         }
+        my $state_iso_code = $state->state_iso_code;
         unless ($state_iso_code eq 'NY') {
             return 0;
         }
