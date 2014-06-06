d33efc4f81ff34f17ca7308f388a848f1d8ad7b5
Sam Batschelet <sbatschelet@mac.com>
2014-06-06 12:51:59 -0400

    Migrate code from using DBIC to DBI through DPD

--- applications/Angler/lib/Angler/Inventory.pm
+++ applications/Angler/lib/Angler/Inventory.pm
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 use POSIX qw(strftime);
 
-my $now = strftime "%F %H:%M:%S", localtime;
+my $now = strftime "%F %H:%M:%S.000", localtime;
 
-=head2 search_qbpos_sales_receipt($schema, $start, $end)
+=head2 search_qbpos_sales_receipt($dbh, $start, $end)
 
 Input a timestamp $start and $end this method will return a hash with the following
 SalesReceiptItemLine data for items that have been sold during this timeframe.
@@ -18,40 +18,26 @@ As these items have been sold each of these items should be removed from invento
 =cut
 
 sub search_qbpos_sales_receipts {
-    my ($schema, $start, $end) = @_;
-    unless ($schema && $start) {
-        die "Submodule search_qbpos_sales_receipts requires both schema and start time";
+    my ($dbh, $start, $end) = @_;
+    unless ($dbh && $start) {
+        die "Submodule search_qbpos_sales_receipts requires both dbh and start inputs";
     }
     unless ($end) {
         my $end = $now;
     }
     # This query because of the nathure of the qbpos driver could take 30 mins to run.
-    my $sales_reciept_rs = $schema->resultset('SalesReceiptItem')->search({
-        -and => [
-            TimeCreated => { '>=', $start },
-            TimeCreated => { '<=', $end },
-        ],
-    });
+    my $sth = $dbh->prepare("select salesreceiptitemupc as upc, salesreceiptitemalu as alu,
+        salesreceiptnumber as sales_receipt_number, itemscount as qty, timecreated as created,
+        salesreceiptitemlistid as item_id from SalesReceiptItem where TimeCreated >= {ts '$start'} AND TimeCreated <= {ts '$end'}");
 
-    my @receiptlines;
+    $sth->execute;
 
-    while (my $record = $sales_receipt_rs->next) {
-        #inflate
-        my $receipt_item = {$record->get_inflated_columns};
-        push @saleslines,
-            {
-                upc => $item->{salesreceiptitemupc},
-                alu => $item->{salesreceiptitemalu},
-                sales_reciept_number => $item->{salesreceiptnumber},
-                qty => $item->{itemscount},
-                created => $item->{timecreated},
-                item_id => $item->{salesreceiptitemlistid}
-             };
-    };
+    my @receiptlines = $sth->fetchall_arrayref({});
+
     return \@receiptlines;
 }
 
-=head2 search_qbpos_sales_orders($schema, $start, $end)
+=head2 search_qbpos_sales_orders($dbh, $start, $end)
 
 This is very simular to sales_receipt_sync except this method searches recent open
 sales orders for inventory items.  Although these items have not been paid for yet they
@@ -60,38 +46,21 @@ need to be removed from current available inventory and put on hold.
 =cut
 
 sub search_qbpos_sales_orders {
-    my ($schema, $start, $end) = @_;
+    my ($dbh, $start, $end) = @_;
 
-    unless ($schema && $start) {
-        die "Submodule search_qbpos_sales_orders requires both schema and start time";
+    unless ($dbh && $start) {
+        die "Submodule search_qbpos_sales_orders requires both dbh and start inputs";
     }
     unless ($end) {
         my $end = $now;
     }
     # items that are on open sales orders also need to be considered sold.
-    my $sales_order_item_rs = $schema->resultset('SalesOrderItem')->search({
-        -and => [
-            TimeCreated => { '>=', $start },
-            TimeCreated => { '<=', $end },
-            SalesOrderStatusDesc => { '=', 'Open' },
-        ],
-    });
+    my $sth = $dbh->prepare("select salesreceiptitemupc as upc, salesreceiptitemalu as alu,
+        salesreceiptnumber as sales_reciept_number, itemscount as qty, timecreated as created,
+        salesreceiptitemlistid as item_id from SalesOrderItem where TimeCreated >= {ts '$start'} AND TimeCreated <= {ts '$end'} AND salesorderstatusdesc = 'Open'");
 
-    my @solines;
+    my @solines = $sth->fetchall_arrayref({});
 
-    while (my $record = $sales_order_rs->next) {
-        #inflate
-        my $so_item = {$record->get_inflated_columns};
-        push @solines,
-            {
-                upc => $item->{salesreceiptitemupc},
-                alu => $item->{salesreceiptitemalu},
-                sales_reciept_number => $item->{salesreceiptnumber},
-                qty => $item->{itemscount},
-                created => $item->{timecreated},
-                item_id => $item->{salesreceiptitemlistid}
-             };
-    };
     return  /@solines;
 }
 
