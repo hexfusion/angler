54da250756336c3157fda3ac82f89c3a745e84a0
Sam Batschelet <sbatschelet@mac.com>
2014-10-25 12:18:32 -0400

    remove cruft add video iterator for product display

--- applications/Angler/lib/Angler.pm
+++ applications/Angler/lib/Angler.pm
@@ -106,22 +106,7 @@ hook 'before_navigation_display' => sub {
 hook 'before_product_display' => sub {
     my ($tokens) = @_;
 
-    # implement a flash message
-    if (my $msg = session('flypage_message')) {
-        session flypage_message => undef;
-        $tokens->{messages} = $msg;
-    }
-
     my $product = $tokens->{product};
-    my $user = logged_in_user;
-    my $user_id = session('logged_in_user_id');
-    my $sku = $product->sku;
-
-    my $status = logged_in_user;
-
-    my $path = $product->path('menu');
-
-    my $current_nav = pop @$path;
 
     # order quantity
     my $qmin = 1;
@@ -134,146 +119,20 @@ hook 'before_product_display' => sub {
     my $free_shipping_amount = config->{free_shipping}->{amount};
     
     if ($product->price > $free_shipping_amount) {
-	$tokens->{free_shipping} = 1;
+    $tokens->{free_shipping} = 1;
     }
-
-    my @other_products;
-#    my @review_list;
-
-    # set form review defaults
-#    my $form = form('review');
-#
-#    $form->{values}->{rating} //= '0';
-#    my $values = $form->{values};
-    # debug "Errors: ", $form->{errors};
-#    $form->fill($values);
-#    $form->action('/review/' . $product->sku);
-#    $tokens->{form} = $form;
-#    $tokens->{errors} = session('review_errors');
-#    session review_errors => undef;
-
-#    if ($user) {
-#        $tokens->{review_link} = '#open';
-#        $tokens->{review_nickname} = shop_user($user_id)->nickname;
-#    }
-#    else {
-#        $tokens->{review_link} = '/login';
-#    }
-
-#    $tokens->{disable_review} = rset('Review')->find({sku => $sku, users_id => $user_id});
-
-    # create review iderators
-#    my $review_rs = shop_review->search({sku => $sku, approved => '1', public => '1'});
-#
-#    while (my $review = $review_rs->next) {
-#
-#        my $user = shop_user($review->users_id);
-#        debug "Review user_id: ", $user->id;
-#        my $nickname = $user->nickname;
-#        my $first_name = $user->first_name;
-#        my $last_name = substr($user->last_name, 0, 1);
-#        my $avatar = $user->find_attribute_value('user_avatar_thumb') || '/img/img_default_user_icon_50x50.jpg';
-#        debug "Avatar: ", $avatar;
-#        my $reviewer;
-#
-#        if (!$nickname) {
-#            $reviewer = "- $first_name $last_name.";
-#        }
-#        else {
-#            $reviewer = "- $nickname";
-#            $reviewer =~ s/([\w']+)/\u\L$1/g;
-#        }
-#
-#        push @review_list, {'title' => $review->title,
-#                            'content' => $review->content,
-#                            'rating' => $review->rating,
-#                            'recommend' => $review->recommend,
-#                            'avatar' => $avatar,
-#                            'reviewer' => $reviewer,
-#                        };
-#        $tokens->{review_list} = \@review_list;
-#    };
-
-    if ($current_nav) {
-        my $other_records = config->{records}->{other_products};
-
-        my $same_category = $current_nav->search_related(
-            'navigation_products')->search_related(
-                'product', {
-                    'product.active' => 1,
-                    'product.sku' => {'!=' => $product->sku},
-                },
-                {
-                    rows => $other_records,
-                    page => 1,
-                },
-                );
-
-        while (my $product = $same_category->next) {
-            my $product_href = {$product->get_inflated_columns};
-
-            # retrieve thumb and add it to the results
-            my $image = $product->media_by_type('image')->first;
-            if ($image) {
-                $product_href->{image_thumb} = uri_for($image->display_uri('image_50x50'));
-            }
-
-            push @other_products, $product_href;
-        }
-
-        if ($same_category->pager->last_page > 1) {
-            $tokens->{category_more} = $current_nav->uri;
-        }
-
-        $tokens->{category_name} = 'Other ' . $current_nav->name;
-    }
-
-    $tokens->{category_products} = \@other_products;
 
     # add image. There could be more, so we just pick the first
-    my $image = $product->media_by_type('image')->first;
+    my $image = $product->media_by_type('image')->first || 'default.jpg';
+
     if ($image) {
         $tokens->{image_src} = uri_for($image->display_uri('image_325x325'));
         $tokens->{image_thumb} = uri_for($image->display_uri('image_50x50'));
     }
-
-    return;
-
-        # determine category for product
-    my $categories = query->select(join => [qw/navigation code=navigation navigation_products/],
-                                   where => {sku => $product->sku});
-
-    if (@$categories) {
-        $product->{category_name} = $categories->[0]->{name};
-
-        # get other products for this category
-        my $category_products = query->select(join => [qw/navigation_products sku=sku products/],
-                                   fields => [qw/products.sku products.description products.price/],
-                                   where => {navigation => $categories->[0]->{code}});
-
-        $product->{category_products} = $category_products;
-    }
-
-    return;
-
-    # determine variants for product
-    my $variants = query->select(table => 'product_attributes',
-                                 where => {original_sku => $product->sku},
-                                 );
-
-    my %variant_types;
-
-    for (@$variants) {
-        $variant_types{$_->{name}}->{$_->{value}} = 1;
-    }
-
-    # create iterators
-    while (my ($name, $ref) = each %variant_types) {
-        for my $value (keys %$ref){
-            push (@{$product->{"attribute_$name"}}, {value => $value});
-        }
-    }
-
+    
+    my $video = $product->media_by_type('video')->first;
+    debug "Video", $video->uri;
+    $tokens->{video_src} = $video->uri;
 };
 
 hook 'before_cart_display' => sub {
