b753caceffb8a1d498770b14e7986fac96121b38
Sam Batschelet <sbatschelet@mac.com>
2014-11-18 13:57:06 -0500

    Add /start route Add ability to read audio and image json files Allow random sorting of json resultset Create Captcha and Captcha::Image objects

--- applications/AjaxTest/lib/AjaxTest.pm
+++ applications/AjaxTest/lib/AjaxTest.pm
@@ -4,15 +4,106 @@ use Dancer::Plugin::Form;
 use Dancer::Plugin::Interchange6;
 use Dancer::Plugin::Interchange6::Routes;
 use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Ajax;
 use Dancer::Plugin::Auth::Extensible qw(
 logged_in_user authenticate_user user_has_role require_role
 require_login require_any_role
 );
 
+use Digest::SHA1 'sha1_hex';
+use List::Util qw/shuffle/;
+use File::Slurp qw(read_file write_file);
+use JSON::XS;
+use AjaxTest::Captcha;
+use AjaxTest::Captcha::Image;
 our $VERSION = '0.1';
+
+set session_options => {schema => schema};
 
 get '/' => sub {
     template 'index';
+};
+
+#get '/start/:how_many' => sub {
+#    my $self = shift;
+#    my $qty = params->{how_many};
+#    debug "start how_many: ", $qty;
+
+#    my %test;
+
+#    return to_json {"values" => ["04c1516f014d6224ba90","3f7303f0c6068ec1b68d","620f6c1831415e0a41ff","0d7778eb970540dc4100","fe89ecfead566c8c1930"],
+#                        "imageName"=>"Scissors",
+#                        "imageFieldName" => "c3ba1d134026ca2cb62b",
+#                        "audioFieldName" => "ae3cae8c83edf3e09d3d"};
+#};
+
+get '/start/:how_many' => sub {
+    my $self = shift;
+    my $n = params->{how_many};
+    my %data;
+
+    # define basic params or fallback to defaults
+    my $audio_json = config->{captcha}{default_audios};
+    my $image_json = config->{captcha}{default_images};
+    my $assets_path = config->{captcha}{assets_path};
+
+    # attach the session id to captcha
+    $data{sessions_id} = session 'id';
+
+    # FIXME lets do this with a nice loop
+
+    if(defined($audio_json)) {
+        $data{default_images} = $image_json;
+    };
+
+    if(defined($audio_json)) {
+        $data{default_audios} = $audio_json;
+    };
+    
+    if(defined($assets_path)) {
+        $data{assets_path} = $assets_path;
+    };
+
+    my $captcha = AjaxTest::Captcha->new(\%data);
+    my $public_dir = Dancer::FileUtils::path(setting('appdir'), 'public');
+
+    my $filename = ($public_dir . '/' . $captcha->{assets_path} . '/' .  $captcha->{default_images});
+
+    my $json = -e $filename ? read_file $filename : '{}';
+    # debug "Json file : ", $json;
+
+    my @decoded_json = decode_json($json);
+
+    my @shuffled = shuffle(@{$decoded_json[0]});
+    # debug "Shuffeled: ", \@shuffled;
+
+    # populate random images from dataset
+    for my $j (1 .. $n) {
+        for my $aref (\@shuffled) {
+
+            # add sha1_hex(12) value
+            $aref->[$j]{value} = substr(sha1_hex( $aref->[$j]{name} ), 0, 12);
+
+            # add image to captcha
+            $captcha->add(\%{$aref->[$j]});
+        }
+    }
+
+};
+
+get '/image/:index' => sub {
+    my $self = shift;
+    my $index = params->{index};
+
+
+    debug "image index: ", $index;
+};
+
+get '/:audio:r:type' => sub {
+    my $self = shift;
+    my $file_name = params->{r};
+    my $type = params->{type} || undef;
+    debug "audio file_name type: ", $file_name , $type;
 };
 
 true;
