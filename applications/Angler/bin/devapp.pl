41cccfa20c675153c4ab596aad9cca311f91fdae
Sam Batschelet <sbatschelet@mac.com>
2014-12-31 08:16:21 -0500

    Add ability to use Plack::Middleware::Debug::DBIC::QueryLog via debug panel. Thanks @SysPete

---
+++ applications/Angler/bin/devapp.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use Plack::Builder;
+use Plack::Middleware::Debug::DBIC::QueryLog;
+use Plack::Middleware::DBIC::QueryLog;
+use Dancer;
+use Dancer::Plugin::DBIC;
+use Dancer::Handler;
+use lib 'lib';
+use Angler;
+
+# enables Plack::Middleware::DBIC::QueryLog via debug panel
+my $app = sub {
+    load_app "Angler";
+    Dancer::App->set_running_app("Angler");
+    my $env = shift;
+    my $schema = schema->clone;
+    my $querylog =
+      Plack::Middleware::DBIC::QueryLog->get_querylog_from_env($env);
+    $schema->storage->debug(1);
+        $schema->storage->debugobj($querylog);
+    Dancer::Handler->init_request_headers($env);
+    my $req = Dancer::Request->new( env => $env );
+    Dancer->dance($req);
+};
+
+builder {
+    enable 'Debug',
+      panels => [
+        'Parameters',                'Dancer::Version',
+        'Dancer::Settings',          'Dancer::Logger',
+        'Dancer::TemplateVariables', 'DBIC::QueryLog',
+      ];
+    mount "/" => $app;
+};
