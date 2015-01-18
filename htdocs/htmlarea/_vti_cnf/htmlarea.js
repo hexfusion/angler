2417bec22310d72aab363dc3903c55728af4e854
Sam Batschelet <sbatschelet@mac.com>
2015-01-18 07:21:46 -0500

    remove heavy files from oldfiles branch to increase clone speed.

--- htdocs/htmlarea/_vti_cnf/htmlarea.js
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|20 Oct 2004 16:19:54 -0000
-vti_extenderversion:SR|5.0.2.2623
-vti_usagebymonth:UX|3 146 61
-vti_usagebyweek:UX|2 9 5 92 1
-vti_usagebyday:UX|0 2 0 0 0 0 0 1
-vti_usagelastupdated:TX|08 Dec 2004 09:02:17 -0000
-vti_usagetotalhits:IX|210
--- htdocs/test_code/cool_menu/Samples/Ready to use/Popups/tree_nodes.js
+++
@@ -1,6 +0,0 @@
-// Tree structure definition
-var TREE_NODES = [
-	["Non-JavaScript", "http://javascript.cooldev.com/", "_blank"],
-	["JavaScript", "javascript:void(window.open('http://javascript.cooldev.com/', '_blank', 'width=400,height=300'))", null],
-	["JavaScript, modal dialog (Internet Explorer only)", "javascript:void(window.showModalDialog('http://javascript.cooldev.com/', '_blank', 'dialogWidth=400px;dialogHeight=300px'))", null]
-];
