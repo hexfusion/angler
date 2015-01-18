c06944f462fc247c35e14ed6c5a688190fba7fac
Sam Batschelet <sbatschelet@mac.com>
2015-01-18 15:02:47 -0500

    fix template path.

--- applications/Angler/lib/Angler/Routes/Contact.pm
+++ applications/Angler/lib/Angler/Routes/Contact.pm
@@ -7,7 +7,5 @@ use Dancer::Plugin::Form;
 get '/contact' => sub {
     my $form = form('contact');
 
-    template 'contact', {layout_noleft => 1,
-        layout_noright => 1,
-        form => $form};
+    template 'contact/content', {form => $form};
 };
