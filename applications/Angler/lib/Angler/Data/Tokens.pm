f06f6baf4d87e6d919e7a6c7bcb587ef98be6309
Sam Batschelet <sbatschelet@mac.com>
2015-02-10 06:50:04 -0500

    add Token class which offers generic token data used in various areas.

---
+++ applications/Angler/lib/Angler/Data/Tokens.pm
@@ -0,0 +1,139 @@
+package Angler::Data::Tokens;
+
+use strict;
+use warnings;
+
+use Moo;
+use Data::Transpose 0.0012;
+use Data::Transpose::Prefix;
+
+has schema => (
+    is => 'ro',
+    required => 1,
+);
+
+has form => (
+    is => 'ro',
+);
+
+has country => (
+    is => 'ro',
+);
+
+=head1 DESCRIPTION
+
+This is a collection of useful form tokens
+
+=cut
+
+=head2 checkout
+
+these are the default tokens used in checkout form and include billing and shipping states, countries, card_months and card_years
+
+=head2 countries
+
+input schema and return country resultset
+
+=cut
+
+sub checkout {
+    my ($self) = @_;
+    my $values = $self->form->{values};
+    my $tokens;
+
+    $tokens->{'countries'} = $self->countries;
+    $tokens->{'states'} = $self->_states;
+    $tokens->{'card_months'} = $self->card_months;
+    $tokens->{'card_years'} = $self->card_years;
+
+    return $tokens;
+
+}
+
+sub countries {
+    my ($self) = @_;
+
+    my $countries = [ $self->schema->resultset('Country')->search(
+        {active => 1},
+        {
+          group_by => [ qw/priority name/ ],
+          order_by => { -desc => 'priority' }
+        }
+    )];
+
+    return $countries;
+};
+
+=head2 states
+
+Returns an array reference of active State result rows ordered by name for
+the requested country.
+
+=cut
+
+sub states {
+    my ($self) = @_;
+    my $values = $self->form->{values};
+
+    my $states = [ $self->schema->resultset('State')->search(
+            {country_iso_code => $self->country},
+             active => 1,
+            },
+            {order_by => 'name'},
+    )];
+    return $states;
+};
+
+=head2 _states
+
+internal only see states
+
+=cut
+
+sub _states {
+    my ($self) = @_;
+    my $values = $self->form->{values};
+
+    my $states = [ $self->schema->resultset('State')->search(
+            {country_iso_code => $values->{billing_country},
+             active => 1,
+            },
+            {order_by => 'name'},
+    )];
+    return $states;
+};
+
+=head2 card_months
+
+tokens for credit card expiration months
+
+=cut
+
+sub card_months {
+    my @months;
+
+    for my $i (1..12) {
+        push @months, {value => $i, label => $i};
+    }
+
+    return \@months;
+}
+
+=head2 card_years
+
+tokens for credit card years
+
+=cut
+
+sub card_years {
+    my @years;
+    my $cur_year = DateTime->now->year;
+
+    for my $i ($cur_year..$cur_year+20) {
+        push @years, {value => substr($i,0,2), label => $i};
+    }
+
+    return \@years;
+}
+
+1;
