e206b82d2317edbebcf94e5625eb2f545f8ecb30
Sam Batschelet <sbatschelet@mac.com>
2015-02-21 15:08:22 -0500

    add fetch_pages, fetch_data, and parse_data methods.

--- applications/Angler/lib/Angler/Drone/Schema/ResultSet/SimmsProduct.pm
+++ applications/Angler/lib/Angler/Drone/Schema/ResultSet/SimmsProduct.pm
@@ -8,6 +8,7 @@ Interchange6::Schema::ResultSet::Product
 
 =cut
 
+use open ':std', ':encoding(UTF-8)';
 use WWW::Mechanize;
 use XML::Twig;
 use Try::Tiny;
@@ -22,26 +23,45 @@ Provides extra accessor methods for L<Angler::Drone::Schema::ResultSet::SimmsPro
 
 use strict;
 use warnings;
+use constant { true => 1, false => 0};
 
 use parent 'Angler::Drone::Schema::ResultSet';
 
+=head1 METHODS
 
-sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s }
-sub number { my $s = shift; return undef if $s eq 'N/A'; $s =~ s/,//g; return $s }
-sub printt { $|++; print localtime().' '.shift."\n" }
+=cut
+
+sub trim {
+    my $self = shift;
+    $self =~ s/^\s+|\s+$//g;
+    return $self
+}
+
+sub number {
+    my $self = shift;
+    return undef if $self eq 'N/A';
+    $self =~ s/,//g;
+    return $self
+}
+
 sub compact {
-my $s = shift;
-my @parts = split '\n', trim $s;
-$s = join ', ', map {trim $_} @parts;
-return $s
+    my $self = shift;
+    my @parts = split '\n', trim $self;
+    $self = join ', ', map {trim $_} @parts;
+    return $self
 }
+
 sub compact_address {
-my $s = shift;
-my @parts = split ' ', trim $s;
-$s = join ' ', map {trim $_} @parts;
-return $s;
+    my $self = shift;
+    my @parts = split ' ', trim $self;
+    $self = join ' ', map {trim $_} @parts;
+    return $self;
 }
-sub img_value { my $s = trim shift; return substr $s, -5, 1}
+
+sub img_value {
+    my $self = trim shift;
+    return substr $self, -5, 1
+}
 
 =head2 _init_mech
 
@@ -54,12 +74,73 @@ sub _init_mech {
 
     return $mech, $twig;
 }
-
-=head2 fetch_simms
+=head2 fetch_pages
 
 =cut
 
-sub fetch_simms {
+sub fetch_pages {
+    my $self = shift;
+
+    my (@podjetja, $url, $response, $content, $last);
+    my ($mech, $twig) = $self->_init_mech();
+    my $podjetje_link_base = 'http://www.simmsfishing.com/catalogsearch/result/?q=a';
+    my $total_added = 0;
+
+    my $page = 1;
+    while(true){
+        # URL hunt
+        print "Page  $page:";
+        $url = "http://www.simmsfishing.com/catalogsearch/result/index/?p=$page&q=a";
+        try {
+            $mech->get( $url );
+        }
+        catch {
+            $last = 1;
+        };
+
+        if($last){
+            $last = undef;
+            last;
+        }
+
+        my $added = 0;
+        my $link_no = 0;
+        my $skipped = 0;
+
+        for my $link ($mech->links){
+            my ( $url, $text, $name, $tag, $base, $attr_href ) = @{$link};
+
+            # Skip non company links
+            next unless ($attr_href->{class} and $attr_href->{class} eq 'product-image');
+
+            $link_no++;
+            my $id = substr $url, 28, -5;
+
+            if ($id and !$self->find($id)){
+                $self->create({
+                    id => $id,  
+                });
+                $added++;
+            }
+            else {
+                $skipped++;
+                #printt("$letter$page $link_no - $url skipped");
+            }
+        }
+        last if $skipped == 3 and !$added;
+        $page++;
+        $total_added += $added;
+        print "$added added and $skipped skipped\n";
+    }
+    Dancer::debug "All done. $total_added added.";
+}
+
+
+=head2 fetch_data
+
+=cut
+
+sub fetch_data {
     my $self = shift;
     my ($mech, $twig) = $self->_init_mech();
 
@@ -107,6 +188,55 @@ sub fetch_simms {
         Dancer::debug "$no ".$p->id." fetched";
         $no++;
     }
+}
+
+=head2 parse_data
+
+=cut
+
+sub parse_data {
+    my $self = shift;
+    my ($mech, $twig) = _init_mech();
+
+    my $fetched = $self->search({html => { '-not' => undef}, description => undef});
+
+    my $no = 1;
+    while(my $p = $fetched->next){
+
+        $twig->parse_html($p->html);
+
+        # Headings
+        my @headings;
+        my $content;
+        for my $n ($twig->find_nodes('//[@id="tab-content-1"]//h3')){
+            push @headings, $n->text_only
+        }
+        for my $n ($twig->find_nodes('//[@id="tab-content-1"]/div')){
+            $content = $n->sprint;
+            last;
+        }
+        my $data;
+        for my $part (split '<h3>', $content){
+            my ($title, $con) = split '</h3>',$part;
+            $title = lc $title;
+            $p->$title(trim $con) if $con;
+        }
+
+        # Videos
+        my @videos;
+        for my $n ($twig->find_nodes('//iframe')){
+            next unless defined $n->att('allowfullscreen');
+            push @videos, $n->att('src');
+        }
+        $p->videos(join ', ', @videos);
+
+        $p->update;
+        Dancer::debug "$no ".$p->id." updated";
+        $no++;
+    }
+
+    Dancer::debug "All done";
+
 }
 
 1;
