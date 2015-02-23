3d4d5d1a8e3001ad6f045ce95d2e337e44801345
Sam Batschelet <sbatschelet@mac.com>
2015-02-23 10:11:52 -0500

    add assessors.

--- applications/Angler/lib/Angler/Populate/Product.pm
+++ applications/Angler/lib/Angler/Populate/Product.pm
@@ -15,7 +15,154 @@ Angler::Populate::Product
 
 =head1 DESCRIPTION
 
+assists in population of the product class
+
 =head1 SYNOPSIS
+
+=head1 ASSESSORS
+
+=cut
+
+=head2 sku
+
+Returns sku of product
+
+=cut
+
+has sku => (
+    is => 'ro',
+    required => 1,
+);
+
+=head2 name
+
+Returns name of product
+
+=cut
+
+has name => (
+    is => 'ro',
+    required => 1,
+);
+
+=head2 short_description
+
+Returns short_description of product
+
+=cut
+
+has short_description => (
+    is => 'ro',
+    required => 1,
+);
+
+=head2 description
+
+Returns description of product
+
+=cut
+
+has description => (
+    is => 'ro',
+    required => 1,
+);
+
+=head2 price
+
+Returns price of product
+
+=cut
+
+has price => (
+    is => 'ro',
+    required => 1,
+);
+
+=head2 uri
+
+Returns uri of product
+
+=cut
+
+has uri => (
+    is => 'ro',
+    required => 1,
+);
+
+=head2 weight
+
+Returns weight of product
+
+=cut
+
+has weight => (
+    is => 'ro',
+    default => 1,
+);
+
+=head2 gtin
+
+Returns gtin of product
+
+=cut
+
+has gtin => (
+    is => 'ro',
+);
+
+=head2 canonical_sku
+
+Returns canonical_sku of product
+
+=cut
+
+has canonical_sku => (
+    is => 'ro',
+);
+
+=head2 active
+
+Returns active 0/1 of product
+
+=cut
+
+has active => (
+    is => 'ro',
+    default => undef
+);
+
+=head2 manufacturer_sku
+
+Returns manufacturer_sku of product
+
+=cut
+
+has manufacturer_sku => (
+    is => 'ro',
+);
+
+=head2 inventory_exempt
+
+Returns inventory_exempt status of product
+
+=cut
+
+has inventory_exempt => (
+    is => 'ro',
+    default => '1'
+);
+
+=head2 priority
+
+Returns priority of product
+
+=cut
+
+has priority => (
+    is => 'ro',
+);
+
+=head1 METHODS
 
 =cut
 
@@ -26,7 +173,6 @@ sub add {
     my $product = $schema->resultset('Product')->find_or_new(
         {
             sku => $self->sku,
-            manufacturer_sku => $self->manufacturer_sku,
             name => $self->name,
             short_description => $self->short_description,
             description => $self->description,
@@ -36,6 +182,7 @@ sub add {
             gtin => $self->gtin,
             canonical_sku => $self->canonical_sku,
             active => $self->active,
+            manufacturer_sku => $self->manufacturer_sku,
             inventory_exempt => $self->inventory_exempt,
             priority => $self->priority
         }
