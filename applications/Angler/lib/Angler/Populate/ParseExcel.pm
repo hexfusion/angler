4a03341ea3724c9b68f4f1a86f633e4881d686f6
Sam Batschelet <sbatschelet@mac.com>
2015-02-25 14:29:37 -0500

    parse method now formats excel data per importer.yml configs.

--- applications/Angler/lib/Angler/Populate/ParseExcel.pm
+++ applications/Angler/lib/Angler/Populate/ParseExcel.pm
@@ -6,7 +6,7 @@ Angler::Populate::ParseExcel
 
 =head1 DESCRIPTION
 
-This module provides population capabilities for the Simms manufacturer
+This module parses and maps excel data file based on importer.yml configs.
 
 =cut
 
@@ -16,12 +16,9 @@ use warnings;
 
 use Dancer ':syntax';
 use Moo;
-use MooX::Types::MooseLike::Base qw(ArrayRef HashRef Int);
 use Spreadsheet::ParseExcel;
 use Spreadsheet::ParseXLSX;
 use Data::Dumper;
-use YAML qw/LoadFile/;
-use File::Spec;;
 
 set logger => 'console';
 set log    => 'info';
@@ -50,12 +47,9 @@ has importer_config => (
 sub parse {
     my ($self) = @_;
 
-    my $manufacturer = 'simms';
+    my $config = $self->importer_config;
 
-    my $config =
-      LoadFile( File::Spec->catfile( config->{appdir}, "importer.yml" ) );
-
-    $config = $config->{manufacturers}->{$manufacturer};
+    info "config $config";
 
     my $parser =
       $config->{type} eq 'xlsx'
@@ -63,14 +57,12 @@ sub parse {
       : Spreadsheet::ParseExcel->new;
 
     # parse the file
-
     my $workbook = $parser->parse($self->{file});
     if ( !defined $workbook ) {
         die $parser->error(), ".\n";
     }
 
     # we need at least one worksheet
-
     die "no worksheets found" unless $workbook->worksheet_count;
 
     my $worksheet = $workbook->worksheet( $config->{worksheet} );
@@ -80,52 +72,25 @@ sub parse {
     my ( $col_min, $col_max ) = $worksheet->col_range();
     my ( $row_min, $row_max ) = $worksheet->row_range();
 
-    info "col min $col_min $col_max";
+   # check headers
+        # grab headers
+        my %headers;
+        foreach my $col ( $col_min .. $col_max ) {
+            $headers{$col} = &trim(@{$config->{headers}}[$col]->{map}); 
+        }
 
-    # grab headers
-    my %headers;
-    foreach my $col ( $col_min .. $col_max ) {
-        $headers{$col} =
-           $worksheet->get_cell( $config->{header_row}, $col )->value;
-    }
-
-    my @map = (
-        { source => 'Product Group Code', action => 'map', result => 'code'},
-        { source => 'SKU', action => 'map', result => 'manufacturer_sku'},
-        { source => 'Product Name - Display', action => 'map', result => 'name'},
-        { source => 'SRP', action => 'map', result => 'price'},
-        { source => 'Color', action => 'map', result => 'color'},
-        { source => 'Size', action => 'map', result => 'size'},
-        { source => 'UPC', action => 'map', result => 'gtin'},
-        { source => 'Item Category Code', action => 'map', result =>'navigation'},
-        { source => 'Season', action => 'map', result => 'season'},
-        { source => 'Base', action => 'delete' },
-        { source => 'Product Name - Description', action=> 'delete' }
-    );
-
-    my @values;
-
-   foreach my $row ( 2 .. $row_max ) {
+    my @data;
+    foreach my $row ( 2 .. $row_max ) {
         my %cells =
           map {
-            $headers{$_} => $worksheet->get_cell( $row, $_ )->value 
+            $headers{$_} => &trim( $worksheet->get_cell( $row, $_ )->value )
           } $col_min .. $col_max;
+            # remove if map = delete
+            delete $cells{'remove_field'};
+            push @data, \%cells;
 
-
-    foreach (@map) {
-        if ($_->{action} eq 'delete') {
-            delete $cells{$_->{source}};
-        }
-        elsif ($_->{action} eq 'map'){
-            $cells{ $_->{result} } = $cells{$_->{source}};
-            delete $cells{$_->{source}};
-        }
-    }
-    push @values, \%cells;
-    }
-    #print Dumper($cells);
-print Dumper(\@values);
-
+     }
+    return \@data;
 }
 
 sub trim {
