5578ee18a4e73eb4c8339c81bb3a8dcb3e27dea6
Peter Mottram <peter@sysnix.com>
2015-03-05 10:46:48 -0500

    validation moved out of Routes::Validator into Validator

--- applications/Angler/lib/Angler/Routes/Validator.pm
+++ applications/Angler/lib/Angler/Routes/Validator.pm
@@ -1,10 +1,8 @@
 package Angler::Routes::Validator;
 
 use Dancer ':syntax';
-use Angler::Interchange6::Schema;
-use Dancer::Plugin::Interchange6;
 use Dancer::Plugin::Ajax;
-use Dancer::Plugin::DBIC;
+use Angler::Validator;
 
 =head1 ROUTES
 
@@ -19,53 +17,23 @@ prefix '/validator';
 Expected params are C<country> which should be ISO country code
 and C<postal_code>.
 
-Returns 200 OK for all countries except US. For US it checks zip3 against zones.
-
 =cut
 
 ajax '/postcode_for_country' => sub {
 
-    my %query = params('query');
-
-    if (   $query{country}
-        && uc( $query{country} ) eq 'US'
-        && $query{postal_code} )
+    if (
+        Angler::Validator::country_and_postal_code(
+            param('country'), param('postal_code')
+        )
+      )
     {
-
-        return fail_response( 404, "invalid zipcode" )
-          unless $query{postal_code} =~ /^(\d{3})\d{2}$/;
-
-        my $zip3 = $1;
-
-        my $zone = schema->resultset('Zone')->search(
-            {
-                'zone_countries.country_iso_code' => 'US',
-                zone                              => "US postal $zip3",
-
-            },
-            {
-                join => 'zone_countries'
-            }
-        )->single;
-
-        return fail_response( 404, "zipcode not found" ) unless $zone;
+        to_json( { type => "success" } );
     }
-
-    to_json({ type => "success" });
+    else {
+        status 404;
+        to_json( { type => "fail" } );
+    }
 };
 
-=head1 METHODS
-
-=head2 fail_response( $code, $message )
-
-=cut
-
-sub fail_response {
-    my ( $code, $msg ) = @_;
-    status($code);
-    return to_json({ type => "fail", message => $msg });
-}
-
-# paranoia
 prefix undef;
 true;
