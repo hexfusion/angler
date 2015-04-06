988faf2bd43b8d4bac7b3ae972dea3fa64d17d3b
Peter Mottram <peter@sysnix.com>
2015-04-06 13:11:29 -0400

    add with_manufacturer_inventory locally to Product ResultSet
    Also chain this onto listing method.

---
+++ applications/Angler/lib/Angler/Interchange6/Schema/ResultSet/Product.pm
@@ -0,0 +1,94 @@
+package Angler::Interchange6::Schema::ResultSet::Product;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Angler::Interchange6::Schema::ResultSet::Product
+
+=cut
+
+package Interchange6::Schema::ResultSet::Product;
+use Interchange6::Schema::ResultSet::Product;
+use Class::Method::Modifiers;
+
+=head1 METHODS
+
+=head2 with_manufacturer_inventory
+
+Add C<lead_time_min_days>, C<lead_time_max_days> and C<manufacturer_quantity>
+from L<Angler::Interchange6::Schema::Result::Inventory> to query using
+correlated subqueries.
+
+=cut
+
+sub with_manufacturer_inventory {
+    my $self = shift;
+
+    return $self->search(
+        undef,
+        {
+            '+select' => [
+                {
+                    coalesce => [
+
+                        $self->correlate('variants')
+                          ->related_resultset('inventory')
+                          ->get_column('lead_time_min_days')->sum_rs->as_query,
+
+                        $self->correlate('inventory')
+                          ->get_column('lead_time_min_days')->as_query,
+
+                    ],
+                    -as => 'lead_time_min_days',
+                },
+                {
+                    coalesce => [
+
+                        $self->correlate('variants')
+                          ->related_resultset('inventory')
+                          ->get_column('lead_time_max_days')->sum_rs->as_query,
+
+                        $self->correlate('inventory')
+                          ->get_column('lead_time_max_days')->as_query,
+
+                    ],
+                    -as => 'lead_time_max_days',
+                },
+                {
+                    coalesce => [
+
+                        $self->correlate('variants')
+                          ->related_resultset('inventory')
+                          ->get_column('manufacturer_quantity')
+                          ->sum_rs->as_query,
+
+                        $self->correlate('inventory')
+                          ->get_column('manufacturer_quantity')->as_query,
+
+                    ],
+                    -as => 'manufacturer_quantity',
+                },
+            ],
+            '+as' => [
+                'lead_time_min_days',
+                'lead_time_max_days',
+                'manufacturer_quantity'
+            ],
+        }
+    );
+}
+
+=head2 listing
+
+Add L</with_manufacturer_inventory> to
+L<Interchange6::Schema::Result::Inventory/listing>.
+
+=cut
+
+around 'listing' => sub {
+    return shift->(@_)->with_manufacturer_inventory;
+};
+
+1;
