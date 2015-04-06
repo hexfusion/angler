26f14a5c656d45d6423f4a463c026473f7b8b072
Peter Mottram <peter@sysnix.com>
2015-04-06 13:12:39 -0400

    add manufacturer_quantity to Inventory result class

--- applications/Angler/lib/Angler/Interchange6/Schema/Result/Inventory.pm
+++ applications/Angler/lib/Angler/Interchange6/Schema/Result/Inventory.pm
@@ -38,12 +38,16 @@ numeric value representing number of products available from manufacturer
 
 __PACKAGE__->add_columns(
     lead_time_min_days => {
-        data_type      => "integer",
-        default_value  => 0
+        data_type     => "integer",
+        default_value => 0
     },
     lead_time_max_days => {
-        data_type      => "integer",
-        default_value  => 0
+        data_type     => "integer",
+        default_value => 0
+    },
+    manufacturer_quantity => {
+        data_type     => "integer",
+        default_value => 0
     },
     manufacturer_quantity  => {
         data_type      => "integer",
