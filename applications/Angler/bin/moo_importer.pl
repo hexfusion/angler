0d70c1e473bef54a243b019b431ae5ac49349f19
Sam Batschelet <sbatschelet@mac.com>
2015-04-28 16:01:28 -0400

    add inventory population from excel files.

--- applications/Angler/bin/moo_importer.pl
+++ applications/Angler/bin/moo_importer.pl
@@ -18,6 +18,7 @@ use Angler::Populate::ProductVariant;
 use Angler::Populate::Attribute;
 use Angler::Populate::ParseExcel;
 use Angler::Populate::Image;
+use Angler::Populate::Inventory;
 use File::Basename;
 use File::Path qw/make_path/;
 use File::Copy;
@@ -61,7 +62,7 @@ my $exp_dir = config->{appdir} . '/export';
 
 my @img_sizes = qw/35 75 100 110 200 325 975/;
 
-my ( $active, $export, $file, $help, $manufacturer, %nav_lookup );
+my ( $active, $inventory, $export, $file, $help, $manufacturer, %nav_lookup );
 
 
 GetOptions(
@@ -70,6 +71,7 @@ GetOptions(
     "help"           => \$help,
     "manufacturer=s" => \$manufacturer,
     "export"         => \$export,
+    "inventory"      => \$inventory
 );
 pod2usage(1) if $help;
 
@@ -98,6 +100,7 @@ unless ( defined $config->{manufacturers}->{$manufacturer}->{type} ) {
 
 $active = 0 unless $active;
 $export = 0 unless $export;
+$inventory = 0 unless $inventory;
 # parse by type
 
 my $type = $config->{manufacturers}->{$manufacturer}->{type};
@@ -139,22 +142,24 @@ if ( $type =~ /^xls/ ) {
         $drone_data = $drone_rs->count;
     }
 
-    my %seen;
-    my $product;
-    my $drone_product;
+    my ($product, $worksheet, $drone_product);
 
-    # Create a new Excel workbook
-    my $workbook = Spreadsheet::WriteExcel->new($exp_dir . '/'. $manufacturer . $now .  '.xls');
-    my $worksheet = $workbook->add_worksheet();
-    my @excel_export = (
-        ['canonical_desc', 'variant_desc', 'alu', 'upc', 'department_code', 'income_account', 'cogs_account',
-        'attribute', 'size', 'vendor_code', 'vendor_name', 'avg_cost', 'order_cost', 'reg_price', 'msrp', 'item_type',
-        'asset_account', 'custom_field_1']
-    );
+    # if we are using the exporter lets create the excel file now
+    if ($export) {
+        # Create a new Excel workbook for export
+        my $workbook = Spreadsheet::WriteExcel->new($exp_dir . '/'. $manufacturer . $now .  '.xls');
+        my $worksheet = $workbook->add_worksheet();
+        my @excel_export = (
+            ['canonical_desc', 'variant_desc', 'alu', 'upc', 'department_code', 'income_account', 'cogs_account',
+            'attribute', 'size', 'vendor_code', 'vendor_name', 'avg_cost', 'order_cost', 'reg_price', 'msrp', 'item_type',
+            'asset_account', 'custom_field_1']
+        );
+    }
 
     my @products;
+    my %seen;
 
-    # define caonical products in @data
+    # define canonical products in @data
     my @canonical = grep { ! $seen{$_->{code}}++ } @data;
     foreach (@canonical) {
         if ($drone_class) {
@@ -243,6 +248,8 @@ if ( $type =~ /^xls/ ) {
         $attributes->add;
     }
 
+    my $product_variant;
+
     # add variants
     foreach my $record (@variants) {
         # check config for cells to merge
@@ -255,8 +262,10 @@ if ( $type =~ /^xls/ ) {
 
         if ($manufacturer eq 'patagonia') {
             print "adding image \n";
-             $variant->add_image($img_dir);
+            $variant->add_image($img_dir);
         }
+
+        print "variant sku ", $variant->sku, "\n";
 
         if ($export) {
             my $remove = $variant->clean;
@@ -268,8 +277,30 @@ if ( $type =~ /^xls/ ) {
         }
     }
 
-    # write to file
-    $worksheet->write_col('A1', \@excel_export);
+    if ($export) {
+        # write to file
+        $worksheet->write_col('A1', \@excel_export);
+    }
+
+    if ($inventory) {
+        my ($inventory_file, $inv);
+
+        # check for inventory file
+        if ( $config->{inventory_file} ) {
+            $inventory_file = 
+              File::Spec->catfile( [ File::Spec->splitpath($0) ]->[1],
+              '..', 'shared', 'data',  $config->{inventory_file} );
+            $inv = Angler::Populate::Inventory->new(
+                {
+                    file => $inventory_file,
+                    importer_config => $config
+                }
+            );
+            print "Intiate inventory sync \n";
+            my $total = $inv->sync;
+        }
+    }
+
 
     # check canonical products for image.
     foreach (@products) {
@@ -315,6 +346,7 @@ moo-importer.pl [options]
   -e | --export             set erp file export to 't' (defaults to 'f')
   -f | --file               file to import
   -m | --manufacturer       manufacturer name
+  -i | --inventory          add inventory population
   -h | --help               help message
 
 =cut
