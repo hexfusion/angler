e28090a5ff37ea686b992c56ffbd5068a685e208
Sam Batschelet <sbatschelet@mac.com>
2015-06-27 16:37:06 -0400

    add NavigationRedirect and ProductRedirect local result classes

--- applications/Angler/lib/Angler/Interchange6/Schema.pm
+++ applications/Angler/lib/Angler/Interchange6/Schema.pm
@@ -10,11 +10,12 @@ use warnings;
 The following Angler::Interchange6::Schema classes extend
 Interchange6::Schema
 
-Angler::Interchange6::Schema::Result::MyNewClass
+Angler::Interchange6::Schema::Result::ProductRedirect
+Angler::Interchange6::Schema::Result::NavigationRedirect
 
 =cut
 
-#Angler::Interchange6::Schema->load_classes(qw/ Result::MyNewCLass /);
+Angler::Interchange6::Schema->load_classes(qw/ Result::NavigationRedirect Result::ProductRedirect /);
 
 =head1 OVERRIDE
 
@@ -23,11 +24,13 @@ The following Interchange6::Schema classes are overidden
 Interchange6::Schema::Result::Product
 Interchange6::Schema::Result::Navigation
 Interchange6::Schema::ResultSet::Product
+Interchange6::Schema::ResultSet::Inventory
 
 =cut
 
 use Angler::Interchange6::Schema::Result::Product;
 use Angler::Interchange6::Schema::Result::Navigation;
+use Angler::Interchange6::Schema::Result::Inventory;
 use Angler::Interchange6::Schema::ResultSet::Product;
 
 use base 'Interchange6::Schema';
